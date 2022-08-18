18d48a7c8fafd675a52e9e1f11bc6fede1724011
brett-buskirk <buskirkbrett8@gmail.com>
2022-08-17 21:55:05 -0400

    Initial commit

---
+++ ssh-server.py
@@ -0,0 +1,21 @@
+import os
+import paramiko
+import socket
+import sys
+import threading
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key'))
+
+class Server (paramiko.ServerInterface):
+    def _init_(self):
+        self.event = threading.Event()
+
+    def check_channel_request(self, kind, chanid):
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+
+    def check_auth_password(self, username, password):
+        if (username == 'brett') and (password == 'sekret'):
+            return paramiko.AUTH_SUCCESSFUL
