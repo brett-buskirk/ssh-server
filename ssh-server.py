56bce9e6bc78325b8b4a21977d6f117a05481ffb
brett-buskirk <buskirkbrett8@gmail.com>
2022-08-18 22:24:34 -0400

    Added __main__ block to project code

--- ssh-server.py
+++ ssh-server.py
@@ -19,3 +19,47 @@ class Server (paramiko.ServerInterface):
     def check_auth_password(self, username, password):
         if (username == 'brett') and (password == 'sekret'):
             return paramiko.AUTH_SUCCESSFUL
+
+if __name__ == '__main__':
+    server = '192.168.1.207'
+    ssh_port = 2222
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SQL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((server, ssh_port))
+        sock.listen(100)
+        print('[*] Listening for connection ...')
+        client, addr = sock.accept()
+    except Exception as e:
+        print('[-] Listen failed: ' + str(e))
+        sys.exit(1)
+    else:
+        print('[+] Got a connection!', client, addr)
+
+    bhSession = paramiko.Transport(client)
+    bhSession.add_server_key(HOSTKEY)
+    server = server()
+    bhSession.start_server(server=server)
+
+    chan = bhSession.accept(20)
+    if chan is None:
+        print('*** No channel.')
+        sys.exit(1)
+
+    print('[+] Authenticated!')
+    print(chan.recv(1024))
+    chan.send('Welcome to bh_ssh')
+    try:
+        while True:
+            command=input('Enter command: ')
+            if command != 'exit':
+                chan.send(command)
+                r = chan.recv(8192)
+                print(r.decode())
+            else:
+                chan.send('exit')
+                print('exiting')
+                bhSession.close()
+                break
+    except KeyboardInterrupt:
+        bhSession.close()
